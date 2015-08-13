5d48e602eabeed3217d0f40b5afd344932681d7b
Whitney <whitneywooyy@gmail.com>
2015-08-13 11:38:54 -0600

    Added proptypes validation

--- public/bundle.js
+++ public/bundle.js
@@ -23681,6 +23681,10 @@
 	var UserProfile = React.createClass({
 		displayName: 'UserProfile',
 
+		propTypes: {
+			username: React.PropTypes.string.isRequired,
+			bio: React.PropTypes.object.isRequired
+		},
 		render: function render() {
 			return React.createElement(
 				'div',
@@ -23711,6 +23715,10 @@
 	var Repos = React.createClass({
 		displayName: 'Repos',
 
+		propTypes: {
+			username: React.PropTypes.string.isRequired,
+			repos: React.PropTypes.array.isRequired
+		},
 		render: function render() {
 			return React.createElement(
 				'div',
@@ -23741,6 +23749,10 @@
 	var Notes = React.createClass({
 		displayName: 'Notes',
 
+		propTypes: {
+			username: React.PropTypes.string.isRequired,
+			notes: React.PropTypes.array.isRequired
+		},
 		render: function render() {
 			return React.createElement(
 				'div',
