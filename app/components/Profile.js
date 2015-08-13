606504fc5ee0a02f9225976259da59119d5bc372
Whitney <whitneywooyy@gmail.com>
2015-08-13 13:00:12 -0600

    Successfully adding notes

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -24,6 +24,10 @@ var Profile = React.createClass({
 	componentWillUnmount: function(){
 		this.unbind('notes');
 	},
+	handleAddNote: function(newNote){
+		// this.ref.child(this.getParams().username).set(this.state.notes.concat([newNote])); // for previous version of reactfire
+		this.firebaseRefs['notes'].push({text: newNote})
+	},
 	render: function(){
 		var username = this.getParams().username;
 		return (
@@ -35,7 +39,7 @@ var Profile = React.createClass({
 					<Repos username={username} repos={this.state.repos} />
 				</div>
 				<div className="col-md-4">
-					<Notes username={username} notes={this.state.notes} />
+					<Notes username={username} notes={this.state.notes} addNote={this.handleAddNote} />
 				</div>
 			</div>
 		);
--- public/bundle.js
+++ public/bundle.js
@@ -23631,8 +23631,8 @@
 	var UserProfile = __webpack_require__(200);
 	var Repos = __webpack_require__(201);
 	var Notes = __webpack_require__(202);
-	var ReactFireMixin = __webpack_require__(203);
-	var Firebase = __webpack_require__(204);
+	var ReactFireMixin = __webpack_require__(205);
+	var Firebase = __webpack_require__(206);
 
 	var Profile = React.createClass({
 		displayName: 'Profile',
@@ -23653,6 +23653,10 @@
 		componentWillUnmount: function componentWillUnmount() {
 			this.unbind('notes');
 		},
+		handleAddNote: function handleAddNote(newNote) {
+			// this.ref.child(this.getParams().username).set(this.state.notes.concat([newNote])); // for previous version of reactfire
+			this.firebaseRefs['notes'].push({ text: newNote });
+		},
 		render: function render() {
 			var username = this.getParams().username;
 			return React.createElement(
@@ -23671,7 +23675,7 @@
 				React.createElement(
 					'div',
 					{ className: 'col-md-4' },
-					React.createElement(Notes, { username: username, notes: this.state.notes })
+					React.createElement(Notes, { username: username, notes: this.state.notes, addNote: this.handleAddNote })
 				)
 			);
 		}
@@ -23755,14 +23759,16 @@
 	'use strict';
 
 	var React = __webpack_require__(1);
-	var NotesList = __webpack_require__(205);
+	var NotesList = __webpack_require__(203);
+	var AddNote = __webpack_require__(204);
 
 	var Notes = React.createClass({
 		displayName: 'Notes',
 
 		propTypes: {
 			username: React.PropTypes.string.isRequired,
-			notes: React.PropTypes.array.isRequired
+			notes: React.PropTypes.array.isRequired,
+			addNote: React.PropTypes.func.isRequired
 		},
 		render: function render() {
 			return React.createElement(
@@ -23775,6 +23781,7 @@
 					this.props.username,
 					' '
 				),
+				React.createElement(AddNote, { username: this.props.username, addNote: this.props.addNote }),
 				React.createElement(NotesList, { notes: this.props.notes })
 			);
 		}
@@ -23785,6 +23792,79 @@
 
 /***/ },
 /* 203 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	var React = __webpack_require__(1);
+
+	var NotesList = React.createClass({
+		displayName: "NotesList",
+
+		render: function render() {
+			var notes = this.props.notes.map(function (note, index) {
+				return React.createElement(
+					"li",
+					{ className: "list-group-item", key: index },
+					" ",
+					note.text,
+					" "
+				);
+			});
+			return React.createElement(
+				"ul",
+				{ className: "list-group" },
+				notes
+			);
+		}
+
+	});
+
+	module.exports = NotesList;
+
+/***/ },
+/* 204 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	var React = __webpack_require__(1);
+
+	var AddNote = React.createClass({
+		displayName: "AddNote",
+
+		propTypes: {
+			username: React.PropTypes.string.isRequired,
+			addNote: React.PropTypes.func.isRequired
+		},
+		handleSubmit: function handleSubmit() {
+			var newNote = this.refs.note.getDOMNode().value;
+			console.log(newNote);
+			this.refs.note.getDOMNode().value = '';
+			this.props.addNote(newNote);
+		},
+		render: function render() {
+			return React.createElement(
+				"div",
+				{ className: "input-group" },
+				React.createElement("input", { type: "text", className: "form-control", ref: "note", placeholder: "Add New Note" }),
+				React.createElement(
+					"span",
+					{ className: "input-group-btn" },
+					React.createElement(
+						"button",
+						{ className: "btn btn-default", type: "button", onClick: this.handleSubmit },
+						"Submit"
+					)
+				)
+			);
+		}
+	});
+
+	module.exports = AddNote;
+
+/***/ },
+/* 205 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -24155,7 +24235,7 @@
 
 
 /***/ },
-/* 204 */
+/* 206 */
 /***/ function(module, exports) {
 
 	/*! @license Firebase v2.2.9
@@ -24424,38 +24504,6 @@
 
 	module.exports = Firebase;
 
-
-/***/ },
-/* 205 */
-/***/ function(module, exports, __webpack_require__) {
-
-	"use strict";
-
-	var React = __webpack_require__(1);
-
-	var NotesList = React.createClass({
-		displayName: "NotesList",
-
-		render: function render() {
-			var notes = this.props.notes.map(function (note, index) {
-				return React.createElement(
-					"li",
-					{ className: "list-group-item", key: index },
-					" ",
-					note,
-					" "
-				);
-			});
-			return React.createElement(
-				"ul",
-				{ className: "list-group" },
-				notes
-			);
-		}
-
-	});
-
-	module.exports = NotesList;
 
 /***/ }
 /******/ ]);
